fc2e4ef3539ec44edd9e487512c848bb167f3dec
Angel Lopez Manriquez <sigma271@outlook.com>
2017-10-04 23:53:00 -0500

    Add files via upload

--- java/practice db/PetsDB.java
+++ java/practice db/PetsDB.java
@@ -1,6 +1,8 @@
 
 import java.sql.*;
 import java.util.*;
+import java.awt.*;
+import javax.swing.*;
 
 public class PetsDB {
     private String dbname = "pets",
@@ -8,7 +10,11 @@ public class PetsDB {
                    pass = "", 
                    url = ("jdbc:mysql://localhost:3306/"+dbname);
     private LinkedList<String> tables = new LinkedList<>(),
-                               attributes = new LinkedList<>();
+                               attributes = new LinkedList<>(); 
+    private LinkedList<Object> values = new LinkedList<>();
+    Connection connection;
+    Statement statement;
+    ResultSet result;
 
     public PetsDB() {
         tables.add("dogs");
@@ -17,20 +23,48 @@ public class PetsDB {
         attributes.add("age");
         attributes.add("gender");
 
+        values.add("pika");
+        values.add("pikacho");
+        values.add(99);
+        values.add("hembra");
+
         try {
-            Connection conn = DriverManager.getConnection(url, user, pass);
-            Statement statement = conn.createStatement();
-            ResultSet result = statement.executeQuery("select * from "+tables.get(0));
-            while (result.next()) {
-                System.out.println(result.getString(attributes.get(0))+", "+
-                    result.getString(attributes.get(1))+", "+
-                    result.getString(attributes.get(2))+", "+
-                    result.getString(attributes.get(3)));
-            }
+            connection = DriverManager.getConnection(url, user, pass);
+            statement = connection.createStatement();
+            insertElements();
         } catch (Exception exc) {
             exc.printStackTrace();
         }
+    }
 
+    public void insertElements() throws Exception {
+        String insert = "insert into "+tables.get(0)+"(";
+        for (int i = 0; i < attributes.size(); i++) {
+            insert += attributes.get(i);
+            if (i == attributes.size() - 1)
+                break;
+            insert += ", ";
+        }
+        insert += ") values(";
+        for (int i = 0; i < values.size(); i++) {
+            insert += values.get(i);
+            if (i == values.size() - 1)
+                break;
+            insert += ", ";
+        }
+        insert += ");";
+        System.out.printf("%s", insert);
+        //statement.executeUpdate(insert);
+    }
+
+    public void showAttributes() throws Exception {
+        result = statement.executeQuery("select * from "+tables.get(0));
+        while (result.next()) {
+            System.out.println(result.getString(attributes.get(0))+", "+
+                result.getString(attributes.get(1))+", "+
+                result.getString(attributes.get(2))+", "+
+                result.getString(attributes.get(3)));
+        }
     }
 
     public static void main (String args[]) {
---
+++ java/practice db/PetsGUI.java
@@ -0,0 +1,9 @@
+
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class PetsGUI extends PetsGUI {
+    private JLabel 
+
+}
